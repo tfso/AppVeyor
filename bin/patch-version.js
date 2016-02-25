abca7cd9f4c27074a26ef5eef35b32b38e2f827b
lostwork <ng@24sevenoffice.com>
2016-02-25 10:57:00 +0100

    debug info

--- bin/patch-version.js
+++ bin/patch-version.js
@@ -15,5 +15,6 @@ function getServiceTag() {
 ;
 var dir = process.argv[2] || process.cwd();
 var version = getServiceTag();
+console.log('Trying to patch version %', version);
 patchVersion(dir, version, console.log, console.error, process.exit);
 //# sourceMappingURL=patch-version.js.map
