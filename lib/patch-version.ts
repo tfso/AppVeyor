ab64e3f471e44c3a99eaeafc607d55b06b5ee080
lostfields <ng@24sevenoffice.com>
2016-10-04 15:02:02 +0200

    adding some debugging for patch-version

--- lib/patch-version.ts
+++ lib/patch-version.ts
@@ -2,11 +2,14 @@
 import fsp = require('../lib/fs-promise');
 
 export default function patchVersion(file: string, version: string, log?: Function, error?: Function, exit?: Function): Promise<any> {
-    var packagePath = path.normalize(file);
+    var packagePath = path.normalize(file),
+        fileContent;
 
     return fsp
         .readFile(packagePath)
         .then((content: string) => {
+            fileContent = content;
+
             content = content.replace(/("(?:(?:\\[^\n]|[^""\n])*)")|\/\*(.|\n|\r\n)*?\*\/|(?:\/\/.*$)/mgi, "$1");
             var json = JSON.parse(content);
 
@@ -25,6 +28,13 @@ export default function patchVersion(file: string, version: string, log?: Functi
                 exit(0);
         })
         .catch((exception) => {
+            if (exception.name == "SyntaxError") {
+                if (log) {
+                    log("File content;")
+                    log(fileContent);
+                }
+            }
+            
             if (error != null) {
                 error(exception);
             }
