c027bde3134b248bed5decb9c175731b549d1ee3
lostfields <ng@24sevenoffice.com>
2017-07-04 15:54:16 +0200

    removing expect from test

--- tests/when_running_tests.ts
+++ tests/when_running_tests.ts
@@ -1,12 +1,7 @@
 ﻿import assert = require('assert');
-import expect = require('expect');
 
 describe('When running tests', () => {
     it('should succeed with assert', () => {
         assert.ok(true, "This shouldn't fail");
-    });
-
-    it('should succeed with expect', () => {
-        //expect(true).toBe(true, "This shouldn't fail");
     });
 });
