859c1f52dc2a71a6b115ead1b4aeafd2e1bec896
lostfields <ng@24sevenoffice.com>
2017-07-05 11:23:21 +0200

    fixing correct path for senchacmd

--- bin/sencha.ts
+++ bin/sencha.ts
@@ -9,7 +9,7 @@ import { Command } from './../lib/sencha/command';
 program
     .version(process.env.npm_package_version || require('./../package.json').version)
     .description("Sencha wrapper")
-    .option('-c, --sencha-cmd <path>', 'Path to sencha command, either given by install or environment SENCHACMD, default: ' + (process.env.SENCHACMD || "sencha.exe"), path.normalize, process.env.SENCHACMD || "sencha.exe")
+    .option('-c, --sencha-cmd <path>', 'Path to sencha command, either given by install or environment SENCHACMD, default: ' + (process.env.SENCHACMD || "sencha.exe"), path.normalize, process.env.SENCHACMD)
     .parse(process.argv);
 
 if (program.args.length == 0)
@@ -19,8 +19,6 @@ var SENCHACMD = process.env.SENCHACMD || program['senchaCmd'] || null;
 
 if(SENCHACMD)
     Command.path = SENCHACMD;
-
-process.stdout.write('Using SENCHACMD from "' + Command.path + '"');
 
 new Command()
     .on('stdout', data => process.stdout.write(data))
